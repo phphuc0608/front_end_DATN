887f6cb2656e4d28960a6700437c7dbd59a6e64c
phphuc0608 <phuc31082002@gmail.com>
2024-05-06 17:39:43 +0700

    change IP

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://192.168.1.20:8000/',
+        target: 'http://113.22.86.144:8000/',
         changeOrigin: true,
         secure: false,
       }
