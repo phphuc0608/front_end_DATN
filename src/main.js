eaad5fa54940d98f49f778b93410b8dbbc79d5bb
phphuc0608 <phuc31082002@gmail.com>
2024-05-08 17:30:58 +0700

    add interceptor authorization

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import './style.css'
 import App from './App.vue'
 import router from './routers/router'
 import axios from 'axios'
-
-window.axios = axios; //Add axios to window object to use it in console for testing purposes 
+//Add axios to window object to use it in console for testing purposes 
+window.axios = axios;
+// Add a request interceptor to add the token to the request headers
+axios.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 createApp(App).use(router).mount('#app');
