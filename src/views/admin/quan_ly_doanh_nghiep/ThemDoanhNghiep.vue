758f6fca383d84eb7d6d58c1923636931bd825ac
phphuc0608 <phuc31082002@gmail.com>
2024-05-08 17:34:25 +0700

    add validate form

--- src/views/admin/quan_ly_doanh_nghiep/ThemDoanhNghiep.vue
+++ src/views/admin/quan_ly_doanh_nghiep/ThemDoanhNghiep.vue
@@ -6,27 +6,27 @@
       <div class="row">
         <div class="col-md-6">
           <label class="col-form-label">Mã đơn vị</label>
-          <input v-model="maDonVi" type="text" class="form-control" placeholder="Nhập mã đơn vị">
+          <input v-model="maDonVi" type="text" class="form-control" placeholder="Nhập mã đơn vị" required>
         </div>
         <div class="col-md-6">
           <label for="" class="col-form-label">Tên đơn vị</label>
-          <input v-model="tenDonVi" type="text" class="form-control" placeholder="Nhập tên đơn vị">
+          <input v-model="tenDonVi" type="text" class="form-control" placeholder="Nhập tên đơn vị" required>
         </div>
       </div>
       <div class="row">
         <div class="col-md-6">
           <label for="" class="col-form-label">Email</label>
-          <input v-model="email" type="text" class="form-control" placeholder="Nhập email">
+          <input v-model="email" type="text" class="form-control" placeholder="Nhập email" :class="{'is-invalid': emailError}" :title="emailError ? 'Email không đúng định dạng' : ''" required>
         </div>
         <div class="col-md-6">
           <label for="" class="col-form-label">Số điện thoại</label>
-          <input v-model="soDienThoai" type="text" class="form-control" placeholder="Nhập số điện thoại">
+          <input v-model="soDienThoai" type="text" class="form-control" placeholder="Nhập số điện thoại" :class="{'is-invalid': soDienThoaiError}" :title="soDienThoaiError ? 'Số điện thoại phải đủ 10 ký tự' : ''" required>
         </div>
       </div>  
       <div class="row">
         <div class="col-md-6">
           <label for="" class="col-form-label">Danh mục đơn vị</label>
-          <select v-model="maDanhMucDonVi" class="form-control form-select h-auto wide">
+          <select v-model="maDanhMucDonVi" class="form-control form-select h-auto wide" required>
             <option class="option_item"v-for="danhMucDonVi in danhMucDonVis" :key="danhMucDonVi.ma_danh_muc_don_vi" :value="danhMucDonVi.ma_danh_muc_don_vi">
               {{ danhMucDonVi.ten_danh_muc_don_vi }}
             </option>
@@ -39,26 +39,6 @@
       </div> 
     </form>
   </div>
-  <!-- <div class="modal" tabindex="-1" role="dialog" v-if="showModal">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Confirmation</h5>
-          <button type="button" class="close" @click="showModal = false">
-            <span>&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p>Thêm doanh nghiệp thành công.</p>
-          <p>Bạn có muốn quay lại trang quản lý doanh nghiệp không?</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary" @click="goToQuanLyDoanhNghiep">Đồng ý</button>
-          <button type="button" class="btn btn-secondary" @click="showModal = false">Đóng</button>
-        </div>
-      </div>
-    </div>
-  </div> -->
 </template>
 <script>
 import { ref, onMounted, computed } from 'vue';
@@ -73,11 +53,8 @@ export default {
     const email = ref('');
     const soDienThoai = ref('');
     const maDanhMucDonVi = ref('');
-    // const showModal = ref(false);
-
-    // const goToQuanLyDoanhNghiep = () => {
-    //   router.push('/quan_ly_doanh_nghiep');
-    // };
+    const emailError = ref(false);
+    const soDienThoaiError = ref(false);
 
     const resetForm = () => {
       maDonVi.value = '';
@@ -90,7 +67,7 @@ export default {
     const getDanhMucDonVi = () => {
       axios.get(`/api/danh-muc-don-vi`, {
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         }
       })
       .then(function(response){
@@ -102,7 +79,22 @@ export default {
       });
     };
     
+    const validateEmail = () => {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      emailError.value = !re.test(email.value);
+    };
+
+    const validateSoDienThoai = () => {
+      const re = /^\d{10}$/;
+      soDienThoaiError.value = !re.test(soDienThoai.value);
+    };
+
     const addDoanhNghiep = async() => {
+      validateEmail();
+      validateSoDienThoai();
+      if(emailError.value || soDienThoaiError.value){
+        return;
+      }
       const doanhNghiepData = {
         ma_don_vi: maDonVi.value,
         ten_don_vi: tenDonVi.value,
@@ -114,13 +106,12 @@ export default {
       try {
         const response = await axios.post(`/api/don-vi`, doanhNghiepData, {
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
           }
         });
         console.log(response.data);
         alert('Thêm doanh nghiệp thành công');
         router.push('/quan_ly_doanh_nghiep');
-        //showModal.value = true;
       } catch (error) {
         console.log(error);
         alert('Thêm doanh nghiệp thất bại');
@@ -142,8 +133,8 @@ export default {
       soDienThoai,
       maDanhMucDonVi,
       addDoanhNghiep,
-      // showModal,
-      // goToQuanLyDoanhNghiep
+      emailError,
+      soDienThoaiError
     }
   },
   components: {
