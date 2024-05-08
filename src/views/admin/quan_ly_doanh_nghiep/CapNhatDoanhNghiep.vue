17afe5b9f678b09550124af7775466f98fc80202
phphuc0608 <phuc31082002@gmail.com>
2024-05-08 17:48:01 +0700

    add validate form

--- src/views/admin/quan_ly_doanh_nghiep/CapNhatDoanhNghiep.vue
+++ src/views/admin/quan_ly_doanh_nghiep/CapNhatDoanhNghiep.vue
@@ -6,27 +6,27 @@
       <div class="row">
         <div class="col-md-6">
           <label class="col-form-label">Mã đơn vị</label>
-          <input v-model="maDonVi" type="text" class="form-control" placeholder="Nhập mã đơn vị">
+          <input v-model="maDonVi" type="text" class="form-control" placeholder="Nhập mã đơn vị" required>
         </div>
         <div class="col-md-6">
           <label for="" class="col-form-label">Tên đơn vị</label>
-          <input v-model="tenDonVi" type="text" class="form-control" placeholder="Nhập tên đơn vị">
+          <input v-model="tenDonVi" type="text" class="form-control" placeholder="Nhập tên đơn vị" required>
         </div>
       </div>
       <div class="row">
         <div class="col-md-6">
           <label for="" class="col-form-label">Email</label>
-          <input v-model="email" type="text" class="form-control" placeholder="Nhập email">
+          <input v-model="email" type="text" class="form-control" placeholder="Nhập email" :class="{'is-invalid': emailError}" :title="emailError ? 'Email không đúng định dạng' : ''" required>
         </div>
         <div class="col-md-6">
           <label for="" class="col-form-label">Số điện thoại</label>
-          <input v-model="soDienThoai" type="text" class="form-control" placeholder="Nhập số điện thoại">
+          <input v-model="soDienThoai" type="text" class="form-control" placeholder="Nhập số điện thoại" :class="{'is-invalid': soDienThoaiError}" :title="soDienThoaiError ? 'Số điện thoại phải đủ 10 ký tự' : ''" required>
         </div>
       </div>  
       <div class="row">
         <div class="col-md-12">
           <label for="" class="col-form-label">Danh mục đơn vị</label>
-          <select v-model="maDanhMucDonVi" class="form-control form-select h-auto wide">
+          <select v-model="maDanhMucDonVi" class="form-control form-select h-auto wide" required>
             <option v-for="danhMucDonVi in danhMucDonVis" :key="danhMucDonVi.ma_danh_muc_don_vi" :value="danhMucDonVi.ma_danh_muc_don_vi">
               {{ danhMucDonVi.ten_danh_muc_don_vi }}
             </option>
@@ -54,12 +54,18 @@ export default {
     const email = ref('');
     const soDienThoai = ref('');
     const maDanhMucDonVi = ref(0);
+    const emailError = ref(false);
+    const soDienThoaiError = ref(false);
     const danhMucDonVis = ref([]); // Đảm bảo biến này được khởi tạo và có dữ liệu
 
     const getDonViById = async () => {
       const maDonViParam = route.params.ma_don_vi;
       try {
-        const response = await axios.get(`/api/don-vi/${maDonViParam}`);
+        const response = await axios.get(`/api/don-vi/${maDonViParam}`,{
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
         const donVi = response.data;
         maDonVi.value = donVi.ma_don_vi;
         tenDonVi.value = donVi.ten_don_vi;
@@ -73,14 +79,34 @@ export default {
 
     const getDanhMucDonVi = async () => {
       try {
-        const response = await axios.get(`/api/danh-muc/don-vi`);
+        const response = await axios.get(`/api/danh-muc-don-vi`,{
+        headers: {
+            'Content-Type': 'application/json',
+          }
+        });
         danhMucDonVis.value = response.data;
       } catch (error) {
         console.log(error);
       }
     };
     
+    const validateEmail = () => {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      emailError.value = !re.test(email.value);
+    };
+
+    const validateSoDienThoai = () => {
+      const re = /^\d{10}$/;
+      soDienThoaiError.value = !re.test(soDienThoai.value);
+    };
+
+
     const updateDoanhNghiep = async() => {
+      validateEmail();
+      validateSoDienThoai();
+      if(emailError.value || soDienThoaiError.value){
+        return;
+      }
       const donVi = {
         ma_don_vi: maDonVi.value,
         ten_don_vi: tenDonVi.value,
