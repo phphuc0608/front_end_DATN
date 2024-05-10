9f015ca53e4788440d40feb20792af05e62d948a
phphuc0608 <phuc31082002@gmail.com>
2024-05-10 23:22:03 +0700

    first commit

---
+++ src/views/admin/quan_ly_cua_khau/quan_ly_to_khai/ThemToKhai.vue
@@ -0,0 +1,162 @@
+<template>
+  <NavbarAdmin/>
+  <div id="content" class="container">
+    <h3 class="my-4 text-left">THÊM TỜ KHAI</h3>
+    <form @submit.prevent="addToKhai">
+      <div class="row">
+        <div class="col-md-6">
+          <label class="col-form-label">Email</label>
+          <input v-model="email" type="text" :class="{'is-invalid': emailError}" class="form-control" placeholder="Nhập email" :title="emailError ? 'Email không đúng định dạng' : ''" required readonly>
+        </div>
+        <div class="col-md-6">
+          <label for="" class="col-form-label">Đơn vị đăng ký</label>
+          <select v-model="maDonVi" class="form-control form-select h-auto wide" required>
+            <option v-for="donVi in donVis" :key="donVi.ma_don_vi" :value="donVi.ma_don_vi">
+              {{ donVi.ten_don_vi }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <label for="" class="col-form-label">Vận đơn</label>
+          <select v-model="maToKhai" class="form-control form-select h-auto wide" required>
+            <option v-for="ToKhai in VanDons" :key="ToKhai.ma_van_don" :value="ToKhai.ma_van_don">
+              {{ ToKhai.ma_van_don }} - {{ ToKhai.ten_hang_hoa }}
+            </option>
+          </select>
+        </div>
+      </div> 
+      <div class="d-flex justify-content-start mt-4">
+        <button type="submit" class="btn btn-success me-3"><i class="bi bi-plus-circle"></i> Thêm vận đơn</button>
+        <button type="button" class="btn btn-danger" @click="resetForm"><i class="bi bi-x-circle"></i> Reset</button>
+      </div> 
+    </form>
+  </div>
+</template>
+<script>
+import { ref, onMounted, computed } from 'vue';
+import NavbarAdmin from '../../../../components/NavbarAdmin.vue';
+import axios from 'axios';
+import router from '../../../../routers/router';
+export default {
+  setup(){
+    const donVis = ref([]); 
+    const VanDons = ref([]);
+    const email = ref(localStorage.getItem('savedEmail'));
+    const maDonVi = ref('');
+    const maToKhai = ref('');
+    const emailError = ref(false);
+
+    const resetForm = () => {
+      maDonVi.value = '';
+      maToKhai.value = '';
+    };
+
+    const getdonVis = () => {
+      axios.get(`/api/don-vi`, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(function(response){
+        donVis.value = response.data;
+        console.log(response.data); 
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    };
+    
+    const getVanDons = () => {
+      axios.get(`/api/van-don`, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(function(response){
+        VanDons.value = response.data;
+        console.log(response.data); 
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    };
+
+    const validateEmail = () => {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      emailError.value = !re.test(email.value);
+    };
+
+    const addToKhai = async() => {
+      validateEmail();
+      if (emailError.value) {
+        return;
+      }
+      const toKhaiData = {
+        email_nguoi_dang_ky: email.value,
+        don_vi_dang_ky: maDonVi.value,
+        ma_trang_thai: 1,
+        ma_van_don: maToKhai.value,
+      };
+      console.log(toKhaiData);
+      try {
+        const response = await axios.post(`/api/to-khai`, toKhaiData, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        console.log(response.data);
+        alert('Thêm tờ khai thành công');
+        router.push('/quan_ly_to_khai');
+        //showModal.value = true;
+      } catch (error) {
+        console.log(error);
+        alert('Thêm tờ khai thất bại');
+      }
+    };
+
+
+    onMounted(() => {
+      document.title = "Thêm tờ khai";
+    });
+
+    getdonVis();
+    getVanDons();
+    return {
+      donVis,
+      resetForm,
+      email,
+      maDonVi,
+      maToKhai,
+      VanDons,
+      emailError,
+      validateEmail,
+      addToKhai
+    }
+  },
+  components: {
+    NavbarAdmin
+  },
+}
+</script>
+<style>
+#content{
+  margin-top: 120px;
+}
+input.is-invalid:hover {
+  border-color: #dc3545; /* Red border for error */
+}
+
+/* Optional: Show tooltip on hover */
+input.is-invalid:hover::after {
+  content: attr(title);
+  display: block;
+  position: absolute;
+  background-color: #dc3545;
+  color: #fff;
+  padding: 5px;
+  border-radius: 5px;
+  margin-top: 5px;
+}
+</style>
