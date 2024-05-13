080b64c5cacffdef4723f910a8cb84a8bae48e17
phphuc0608 <phuc31082002@gmail.com>
2024-05-13 22:18:36 +0700

    add savedEmail savedVaiTro savedThuocDonVi

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,11 +3,19 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore({
   id: 'user',
   state: () => ({
-    email: ''
+    email: '',
+    ma_vai_tro: 0,
+    thuoc_don_vi: ''
   }),
   actions: {
     setEmail(newEmail) {
       this.email = newEmail;
+    },
+    setMaVaiTro(newMaVaiTro) {
+      this.ma_vai_tro = newMaVaiTro;
+    },
+    setThuocDonVi(newThuocDonVi) {
+      this.thuoc_don_vi = newThuocDonVi;
     }
   }
 });
