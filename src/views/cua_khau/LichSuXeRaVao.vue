fb5ccde4fc1f4bbbe24fd30384550ca70fece866
phphuc0608 <phuc31082002@gmail.com>
2024-05-15 23:39:52 +0700

    first commit

---
+++ src/views/cua_khau/LichSuXeRaVao.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavbarCuakhau />
+
+</template>
+
+<script>
+import NavbarCuakhau from '../../components/NavbarCuakhau.vue';
+import {ref} from 'vue';
+export default {
+
+  components: {
+    NavbarCuakhau,
+  },
+};
+
+</script>
