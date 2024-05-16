50a11e37f68494bf7d425ddf55fd9fe009fc6171
phphuc0608 <phuc31082002@gmail.com>
2024-05-16 23:52:47 +0700

    first commit

---
+++ src/views/cua_khau/LichSuPhuongTien.vue
@@ -0,0 +1,224 @@
+<template>
+<NavbarCuakhau/>
+<div id="content" class="container-fluid px-5">
+    <h3 class="text-center" style="margin-top: 120px; margin-bottom: 60px;">LỊCH SỬ PHƯƠNG TIỆN</h3>
+  </div>
+  <div class="container-fluid px-5">
+    <div class="header_container">
+      <span class="title">
+        <i class="bi bi-table"></i> Bảng lịch sử tài khoản
+      </span>
+    </div>
+    <div class="table_container p-3">
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Mã lịch sử</th>
+              <th scope="col">Email</th>
+              <th scope="col">Hành động</th>
+              <th scope="col">Thời gian</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(lichSu, index) in paginatedData" :key="index">
+              <td>{{ lichSu.ma_lich_su }}</td>
+              <td>{{ lichSu.bien_so }}</td>
+              <td>{{ lichSu.trang_thai_phuong_tien.ten_trang_thai }}</td>
+              <td>{{ lichSu.thoi_gian }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="col-md-12 d-flex align-items-center justify-content-end flex-wrap px-3 py-2" style="background-color: white;">
+      <nav aria-label="Page navigation example mb-2">
+        <ul class="pagination mb-2 mb-sm-0">
+          <li class="page-item">
+            <a class="page-link" href="#" @click.prevent="previousPage">
+              <i class="bi bi-arrow-left-short"></i>
+            </a>
+          </li>
+          <li class="page-item" v-for="page in totalPages" :key="page">
+            <a class="page-link" href="#" @click.prevent="changePage(page)">{{ page }}</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" @click.prevent="nextPage">
+              <i class="bi bi-arrow-right-short"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { ref, onMounted, computed } from 'vue';
+import NavbarCuakhau from '../../components/NavbarCuakhau.vue';
+import router from '../../routers/router';
+
+export default {
+  setup() {
+    const lichSus = ref([]);
+    const currentPage = ref(1);
+    const itemsPerPage = ref(5);
+
+    onMounted(() => {
+      document.title = "Quản lý tờ khai";
+    });
+
+  const getlichSus = () => {
+    axios.get(`/api/lich-su-phuong-tien`, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(function(response){
+      lichSus.value = response.data;
+      console.log(response.data); 
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+  };
+
+    const paginatedData = computed(() => {
+      const start = (currentPage.value - 1) * itemsPerPage.value;
+      const end = start + itemsPerPage.value;
+      return lichSus.value.slice(start, end);
+    });
+
+    const totalPages = computed(() => {
+      return Math.ceil(lichSus.value.length / itemsPerPage.value);
+    });
+
+    const changePage = (page) => {
+      currentPage.value = page;
+    };
+
+    const nextPage = () => {
+      if (currentPage.value < totalPages.value) {
+        currentPage.value++;
+      }
+    };
+
+    const previousPage = () => {
+      if (currentPage.value > 1) {
+        currentPage.value--;
+      }
+    };
+
+    getlichSus();
+    return{
+      lichSus,
+      paginatedData,
+      totalPages,
+      currentPage,
+      itemsPerPage,
+      changePage,
+      nextPage,
+      previousPage,
+    }
+  },
+
+  components:{
+    NavbarCuakhau
+  },
+} 
+</script>
+
+<style scoped>
+*{
+  font-family: 'Space Grotesk', sans-serif;
+}
+#app{
+  min-height: 100%;
+}
+.box_container{
+  background-color: white;
+  border-radius: 0.5rem;
+  margin-bottom: 1rem;
+}
+.header_container{
+  color: white;
+  padding: 1rem 1.5rem;
+  font-size: 1rem;
+  font-weight: bold;
+  border-top-left-radius: 0.425rem;
+  border-top-right-radius: 0.425rem;
+  border-bottom: 1px solid #dad8d4;
+  background-color: #452B90;
+}
+#search_box_content{
+  background-color: white;
+  padding: 1rem 1.5rem; 
+  border-bottom-left-radius: 0.425rem;
+  border-bottom-right-radius: 0.425rem;
+  border: 1px solid #dad8d4;
+}
+.btn-primary{
+  background-color: #452B90;
+  border-color: #452B90;
+  padding: 10.066px 19.250px;
+}
+.btn-warning{
+  /* padding: 10.066px 19.250px; */
+  color: white;
+}
+.btn-warning:hover{
+  background-color: #ffad26;
+  border-color: #ffad26;
+  color: white;
+}
+.btn-primary:hover{
+  background-color: #321f69;
+  border-color: #321f69;
+}
+.rev_button{
+  background-color: #ffdede;
+  border-color: #ffdede;
+  color: #FF5E5E;
+  /* padding: 10.066px 19.250px */
+}
+.rev_button:hover{
+  background-color: #d37272;
+  border-color: #d37272;
+  color: white;
+}
+.table_container{
+  background-color: white; 
+  border: 1px solid #dad8d4;
+}
+.del_button{
+  background-color: #FF5E5E !important;
+  border-color: #FF5E5E !important;
+  color: white;
+}
+.del_button:hover{
+  background-color: #ff6e6e !important;
+  border-color: #ff6e6e !important;
+  color: white;
+}
+.rev_button{
+  background-color: #ffdede;
+  border-color: #ffdede;
+  color: #FF5E5E;
+  padding: 10.066px 19.250px;
+}
+.rev_button:hover{
+  background-color: #d37272;
+  border-color: #d37272;
+  color: white;
+}
+.pagination .page-item .page-link {
+  text-decoration: none;
+  color: #6e6e6e;
+}
+.pagination .page-item .page-link:hover{
+  background-color: #452B90;
+  color: white;
+  border-color: #452B90; 
+}
+</style>
