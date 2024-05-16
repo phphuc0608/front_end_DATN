96797c4e51f01c88d9f16eab58f80dbf4fef752a
phphuc0608 <phuc31082002@gmail.com>
2024-05-16 23:58:11 +0700

    change template and diagram

--- src/views/cong_ty/thong_ke/ThongKe.vue
+++ src/views/cong_ty/thong_ke/ThongKe.vue
@@ -62,46 +62,119 @@ import router from '../../../routers/router';
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 export default {
   setup() {
-    const boxData1 = ref([
+    const vanDons = ref([]);
+    const maDonVi = ref(localStorage.getItem('savedThuocDonVi'));
+    const totalVanDons = ref(0);
+    const toKhais = ref([]);
+    const totalToKhais = ref(0);
+    const vanDonTheoThang = ref(0);
+    const thangHienTai = ref(new Date().getMonth() + 1);
+    const vanDonTheoQuy = ref(0);
+    const quyHienTai = ref(Math.floor((new Date().getMonth() + 1) / 3) + 1);
+    const toKhaiTheoThang = ref(0);
+    const toKhaiTheoQuy = ref(0);
+    //Computed property sẽ tự động cập nhật khi các reactive dependencies thay đổi.
+    const boxData1 = computed(() => [
       {
         title: 'Tổng số vận đơn',
-        number: 100,
+        number: totalVanDons.value,
         color: '#FFB6C1'
       },
       {
         title: 'Tổng số tờ khai',
-        number: 100,
+        number: totalToKhais.value,
         color: '#FFD700'
       },
       {
         title: 'Vận đơn theo tháng',
-        number: 100,
+        number: vanDonTheoThang.value,
         color: '#FFA07A'
       },
     ]);
-    const boxData2 = ref([
+    const boxData2 = computed(() => [
       {
         title: 'Vận đơn theo quý',
-        number: 100,
+        number: vanDonTheoQuy.value,
         color: '#87CEEB'
       },
       {
         title: 'Tờ khai theo tháng',
-        number: 100,
+        number: toKhaiTheoThang.value,
         color: '#98FB98'
       },
       {
         title: 'Tờ khai theo quý',
-        number: 100,
+        number: toKhaiTheoQuy.value,
         color: '#9370DB'
       }
     ]);
-    const chartDataVanDon = ref({
+    const chartOptions = ref({
+      responsive: true,
+    });
+
+    const getVanDons = async() => {
+      try {
+        const response = await axios.get(`/api/van-don/doanh-nghiep/${maDonVi.value}`);
+        vanDons.value = response.data;
+        totalVanDons.value = vanDons.value.length;
+        console.log(totalVanDons.value)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const getToKhais = async () => {
+      try{
+        const response = await axios.get(`/api/to-khai/doanh-nghiep/${maDonVi.value}`);
+        toKhais.value = response.data;
+        totalToKhais.value = toKhais.value.length;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    
+    const getVanDonTheoThangs = async() => {
+      try{
+        const response = await axios.get(`/api/thong-ke/so-luong-van-don-theo-thang/${maDonVi.value}/${thangHienTai.value}`);
+        vanDonTheoThang.value = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const getVanDonTheoQuys = async() => {
+      try{
+        const response = await axios.get(`/api/thong-ke/so-luong-van-don-theo-quy/${maDonVi.value}/${quyHienTai.value}`);
+        vanDonTheoQuy.value = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const gettoKhaiTheoThangs = async() => {
+      try{
+        const response = await axios.get(`/api/thong-ke/so-luong-to-khai-theo-thang/${maDonVi.value}/${thangHienTai.value}`);
+        toKhaiTheoThang.value = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const gettoKhaiTheoQuys = async() => {
+      try{
+        const response = await axios.get(`/api/thong-ke/so-luong-to-khai-theo-quy/${maDonVi.value}/${quyHienTai.value}`);
+        toKhaiTheoQuy.value = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const chartDataVanDon = computed(()=>({
       labels: ['Vận đơn theo tháng', 'Vận đơn theo quý'],
       datasets: [
         {
-          label: 'Vận đơn', 
-          data: [65, 59],
+          //label: 'Vận đơn', 
+          data: [vanDonTheoThang.value, vanDonTheoQuy.value],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -113,13 +186,13 @@ export default {
           borderWidth: 1
         }
       ],
-    });
-    const chartDataToKhai = ref({
+    }));
+    const chartDataToKhai = computed(()=>({
       labels: ['Tờ khai theo tháng', 'Tờ khai theo quý'],
       datasets: [
         {
           // label: ['Vận đơn', 'Tờ khai'],
-          data: [65, 59],
+          data: [Number(toKhaiTheoThang.value), Number(toKhaiTheoQuy.value)],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -131,9 +204,17 @@ export default {
           borderWidth: 1
         }
       ],
-    });
-    const chartOptions = ref({
-      responsive: true,
+    }));
+    console.log(chartDataToKhai.value);
+
+    onMounted(() => {
+      document.title = "Thống kê";
+      getVanDons();
+      getToKhais();
+      getVanDonTheoThangs();
+      getVanDonTheoQuys();
+      gettoKhaiTheoThangs();
+      gettoKhaiTheoQuys();
     });
     return{
       chartDataToKhai,
@@ -141,6 +222,12 @@ export default {
       chartOptions,
       boxData1,
       boxData2,
+      totalVanDons,
+      totalToKhais,
+      vanDonTheoThang,
+      vanDonTheoQuy,
+      toKhaiTheoThang,
+      toKhaiTheoQuy
     }
   },
 
