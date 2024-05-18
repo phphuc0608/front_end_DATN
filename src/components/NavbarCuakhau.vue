fb3db64da1a9d80e1b31b1291ba557e05f9586a7
phphuc0608 <phuc31082002@gmail.com>
2024-05-18 23:50:30 +0700

    fix responsive bug

--- src/components/NavbarCuakhau.vue
+++ src/components/NavbarCuakhau.vue
@@ -1,5 +1,6 @@
 <template>
   <nav class="container-fluid d-flex justify-content-between align-items-center px-5 py-2">
+    <input type="checkbox" id="check">
     <label for="check" class="checkbtn_sign_out">
       <i class="bi bi-list"></i>
     </label>
