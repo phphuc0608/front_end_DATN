5ad79ddbf087e347e977e8b03444ed16b5e65c5d
phphuc0608 <phuc31082002@gmail.com>
2024-05-22 20:56:16 +0700

    update notification

--- src/views/cong_ty/quan_ly_nguoi_dung/QuanLyNguoiDung.vue
+++ src/views/cong_ty/quan_ly_nguoi_dung/QuanLyNguoiDung.vue
@@ -92,25 +92,7 @@
         </table>
       </div>
     </div>
-    <div class="col-md-12 d-flex align-items-center justify-content-end flex-wrap px-3 py-2" style="background-color: white;">
-      <nav aria-label="Page navigation example mb-2">
-        <ul class="pagination mb-2 mb-sm-0">
-          <li class="page-item">
-            <a class="page-link" href="#" @click.prevent="previousPage">
-              <i class="bi bi-arrow-left-short"></i>
-            </a>
-          </li>
-          <li class="page-item" v-for="page in totalPages" :key="page">
-            <a class="page-link" href="#" @click.prevent="changePage(page)">{{ page }}</a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#" @click.prevent="nextPage">
-              <i class="bi bi-arrow-right-short"></i>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Pagination :totalItems="nguoiDungs.length" :itemsPerPage="itemsPerPage" :currentPage="currentPage" @page-changed="changePage" />
   </div>
 </template>
 
@@ -118,6 +100,8 @@
 import axios from 'axios';
 import { ref, onMounted, computed, watch} from 'vue';
 import NavbarCongty from '../../../components/NavbarCongty.vue';
+import Swal from 'sweetalert2';
+import Pagination from '../../../components/Pagination.vue';
 
 export default {
   setup() {
@@ -158,10 +142,16 @@ export default {
             'Content-Type': 'application/json'
           }
         });  
-        alert('Xóa người dùng thành công');
+        Swal.fire({
+          icon: 'success',
+          title: 'Xóa người dùng thành công',
+        });
         setTimeout(getNguoiDungs, 1000);
       } catch (error) { 
-        alert('Xóa người dùng thất bại');
+        Swal.fire({
+          icon: 'error',
+          title: 'Xóa người dùng thất bại',
+        });
       }
     };
 
@@ -212,27 +202,11 @@ export default {
     const paginatedData = computed(() => {
       const start = (currentPage.value - 1) * itemsPerPage.value;
       const end = start + itemsPerPage.value;
-      return filteredNguoiDungs.value.slice(start, end);
-    });
-
-    const totalPages = computed(() => {
-      return Math.ceil(filteredNguoiDungs.value.length / itemsPerPage.value);
+      return nguoiDungs.value.slice(start, end);
     });
 
     const changePage = (page) => {
       currentPage.value = page;
-    };
-
-    const nextPage = () => {
-      if (currentPage.value < totalPages.value) {
-        currentPage.value++;
-      }
-    };
-
-    const previousPage = () => {
-      if (currentPage.value > 1) {
-        currentPage.value--;
-      }
     };
 
     const filterClicked = () => {
@@ -252,12 +226,9 @@ export default {
     return{
       nguoiDungs,
       paginatedData,
-      totalPages,
       currentPage,
       itemsPerPage,
       changePage,
-      nextPage,
-      previousPage,
       filterClicked,
       deleteNguoiDung,
       searchString,
@@ -270,7 +241,8 @@ export default {
     }
   },
   components:{
-    NavbarCongty
+    NavbarCongty,
+    Pagination
   },
 } 
 </script>
