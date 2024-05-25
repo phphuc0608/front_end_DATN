b2a1e2df431d3f8c42b42c6b79f84be35c0dbb71
phphuc0608 <phuc31082002@gmail.com>
2024-05-25 21:09:22 +0700

    fix link to doi_mat_khau

--- src/components/NavbarCuakhau.vue
+++ src/components/NavbarCuakhau.vue
@@ -27,7 +27,7 @@
           <div class="card-body px-0 py-2">
             <ul class="list-group list-group-flush">
               <li class="card_item list-group-item border-0 py-2">
-                <router-link to="" class="nav-link">
+                <router-link to="/doi_mat_khau" class="nav-link">
                   <i class="bi bi-file-lock"></i>
                   Đổi mật khẩu
                 </router-link>
