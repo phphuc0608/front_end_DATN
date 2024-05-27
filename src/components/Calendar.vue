97f904b8b0eed7b36b9ffc347beafc5e4458199b
phphuc0608 <phuc31082002@gmail.com>
2024-05-27 15:25:07 +0700

    update Calendar features

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -16,70 +16,59 @@
             </option>
           </select>
         </div>
-        </div>
+      </div>
       <div class="calendar-body">
         <div v-for="day in days" :key="day" 
-          :class="['calendar-day', 
-                    { 'current-day': day === today },
-                    { 'min-to-khai': toKhaiCounts[day] === minToKhaiCount },
-                    { 'max-to-khai': toKhaiCounts[day] === maxToKhaiCount }
-                  ]">
-        {{ day }}
-        <div v-if="toKhaiCounts[day]" class="to-khai-count">
-          {{ toKhaiCounts[day] }} tờ khai
+        :class="[
+            'calendar-day p-4',
+            { 'min-to-khai': dayIsMinToKhai(day) }, 
+            { 'max-to-khai': dayIsMaxToKhai(day) }, 
+            { 'no-to-khai': hasNoToKhai(day) } 
+        ]"
+        :style="{ backgroundColor: calculateColor(day) }">
+          <div class="day-label">{{ day }}</div> 
+          <div class="to-khai-count-container" v-if="toKhaiCounts[day]">
+            {{ toKhaiCounts[day] }} tờ khai
+          </div>
         </div>
-      </div>
       </div>
     </div>
   </div>
 </template>
 
 
+
 <script>
 import { computed, onMounted, ref, watch } from 'vue';
+import moment from 'moment';
 
 export default {
   setup() {
     const now = new Date();
-    const currentMonth = ref(now.getMonth()); // Lấy tháng hiện tại
+    const currentMonth = ref(now.getMonth()); 
     const currentYear = ref(now.getFullYear());
     const toKhaiCounts = ref({});
 
     const yearRange = computed(() => {
-      const startYear = now.getFullYear() - 10; // Năm bắt đầu của range
-      const endYear = now.getFullYear() + 10; // Năm kết thúc của range
+      const startYear = now.getFullYear() - 10; 
+      const endYear = now.getFullYear() + 10; 
       const years = [];
       for (let year = startYear; year <= endYear; year++) {
         years.push(year);
       }
       return years;
     });
-
-    const today = computed(() => {
-      const now = new Date();
-      return now.getMonth() === currentMonth.value && now.getFullYear() === currentYear.value
-        ? now.getDate() 
-        : null; // Nếu không phải tháng hiện tại, không highlight ngày nào
-    });
-
   
     const fetchToKhaiCounts = async () => {
       try {
         const startDate = new Date(currentYear.value, currentMonth.value, 1);
         const endDate = new Date(currentYear.value, currentMonth.value + 1, 0);
-        // Clear existing counts before updating
         toKhaiCounts.value = {}; 
         for (let day = startDate.getDate(); day <= endDate.getDate(); day++) {
-            const currentDate = new Date(currentYear.value, currentMonth.value, day);
-            // Format the date string with the 'en-CA' locale for yyyy-mm-dd and UTC timezone
-            const formattedDate = currentDate.toLocaleDateString('en-CA', {
-              year: 'numeric',
-              month: '2-digit',
-              day: '2-digit',
-              timeZone: 'UTC' 
-            });
-            const response = await axios.get(`/api/to-khai/ngay-dang-ky/${formattedDate}/tong-so`);
-            toKhaiCounts.value[day] = response.data; 
+          const currentDate = moment(startDate).date(day); // Tạo moment object
+          const formattedDate = currentDate.format('YYYY-MM-DD');
+          const response = await axios.get(`/api/to-khai/ngay-dang-ky/${formattedDate}/tong-so`);
+          toKhaiCounts.value[day] = response.data; 
         }
       } catch (error) {
           console.error(error);
@@ -102,6 +91,14 @@ export default {
       return result;
     });
 
+    const dayIsMinToKhai = (day) => {
+      return toKhaiCounts.value[day] === minToKhaiCount.value && toKhaiCounts.value[day] !== 0;
+    };
+
+    const dayIsMaxToKhai = (day) => {
+      return toKhaiCounts.value[day] === maxToKhaiCount.value;
+    };
+
     const updateMonth = async () => {
       now.setMonth(currentMonth.value);
       await fetchToKhaiCounts(); // Đợi fetchToKhaiCounts hoàn thành trước khi cập nhật giao diện
@@ -112,7 +109,7 @@ export default {
     };
 
     const minToKhaiCount = computed(() => {
-      const values = Object.values(toKhaiCounts.value);
+      const values = Object.values(toKhaiCounts.value).filter(count => count > 0); // Lọc bỏ các ngày có 0 tờ khai
       return Math.min(...values);
     });
 
@@ -121,16 +118,23 @@ export default {
       return Math.max(...values);
     });
 
+    const hasNoToKhai = (day) => {
+      return !toKhaiCounts.value[day]; 
+    };
+
     onMounted(() => {
       fetchToKhaiCounts();
     });
 
-    watch([currentMonth, currentYear], () => {
-      const now = new Date();
-      today.value = now.getMonth() === currentMonth.value && now.getFullYear() === currentYear.value
-        ? now.getDate() 
-        : null;
-    });
+    const calculateColor = (day) => {
+      if (!toKhaiCounts.value[day] || toKhaiCounts.value[day] === 0) {
+        return '#ccc'; // Màu xám cho các ngày không có tờ khai
+      }
+      const countRange = maxToKhaiCount.value - minToKhaiCount.value;
+      const countRatio = (toKhaiCounts.value[day] - minToKhaiCount.value) / countRange;
+      const hue = 120 - (120 * countRatio); 
+      return `hsl(${hue}, 80%, 70%)`; 
+    };
 
     return {
       currentMonthName,
@@ -138,12 +142,15 @@ export default {
       toKhaiCounts,
       updateMonth,
       currentMonth,
-      today,
       updateYear,
       currentYear,
       yearRange,
       minToKhaiCount,
       maxToKhaiCount,
+      hasNoToKhai,
+      dayIsMaxToKhai,
+      dayIsMinToKhai,
+      calculateColor,
     };
   },
 };
@@ -156,6 +163,7 @@ export default {
   border-radius: 8px;
   font-family: sans-serif;
 }
+
 .calendar-header {
   background-color: #3a496c ;
   padding: 10px;
@@ -163,42 +171,72 @@ export default {
   border-bottom: 1px solid #ccc;
   border-radius: 8px 8px 0 0;
 }
+
 .calendar-header h4 {
   color: white;
 }
+
 .calendar-body {
   display: grid;
   grid-template-columns: repeat(7, 1fr); /* 7 cột cho 7 ngày trong tuần */
 }
+
 .calendar-day {
   text-align: center;
-  padding: 10px;
   border-bottom: 1px solid #eee;
   border-right: 1px solid #eee;
+  position: relative;
 }
+
 .calendar-day:nth-child(7n) { /* Loại bỏ viền phải của ngày cuối tuần */
   border-right: none;
 }
+
+.day-label {
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  font-size: 13px;
+}
+
+.to-khai-count-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-size: 15px; /* Hoặc giá trị khác tùy ý */
+}
+
 .to-khai-count {
   font-size: 12px;
   color: #666;
 }
-.current-day {
-  background-color: #3a496c;
-  color: white;
-}
-.calendar-day.current-day, 
-.calendar-day.current-day .to-khai-count {
-  color: white;
+
+.min-to-khai .to-khai-count, 
+.max-to-khai .to-khai-count {
+    color: white !important; 
 }
 
-.min-to-khai {
-  background-color: #4CAF50; /* Màu xanh lá cây */
-  color: white;
+.min-to-khai, .max-to-khai {
+  color: white; 
+}
+
+.to-khai-count { 
+  font-size: 12px;
+  color: black; 
+  padding: 2px 4px; 
+}
+
+/* .min-to-khai {
+  background-color: #4CAF50;
 }
 
 .max-to-khai {
-  background-color: #f44336; /* Màu đỏ */
-  color: white;
+  background-color: #f44336; 
 }
+
+.no-to-khai {
+  background-color: #ccc; 
+  color: #333; 
+} */
 </style>
